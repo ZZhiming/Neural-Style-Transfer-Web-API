f938360f6a5846fd5874819090264e0d14d6b29a
Zhiming Zhang <zhangzm05@gmail.com>
2019-11-23 18:11:31 -0500

    style image selection

---
+++ static/js/select_style.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+   $( "#image1" ).click(function() {
+  alert( "Handler for .click() called." );
+
+    });
+
+//  $(#image1).onclick
+//  $(":image").css("background-color", "red");
+//  console.log("testing");
+//  //console.log(data.dd);
+//  get_data();
+});
+
+function get_data(){
+     $.ajax({
+        url: '/test/data',
+        success: function(data) {
+            console.log('get info');
+            console.log(data.zhiming)
+//            $('#info').html(JSON.stringify(data, null, '   '));
+//            $('#description').html(data['description']);
+        }
+    });
+}
