f44dfbaf0b24cff61be530e4e5492c923cc64059
Zhiming Zhang <zhangzm05@gmail.com>
2019-11-24 16:03:10 -0500

    select style js

--- static/js/select_style.js
+++ static/js/select_style.js
@@ -1,6 +1,7 @@
+
 $(document).ready(function(){
    $( "#image1" ).click(function() {
-  alert( "Handler for .click() called." );
+  //alert( "Handler for .click() called." );
 
     });
 
@@ -11,9 +12,18 @@ $(document).ready(function(){
 //  get_data();
 });
 
+var prior = null;
 function foo2(e){
+if(this.prior!=null){
+ this.prior.style.opacity = 1
+}
+e.style.opacity = 0.5;
 console.log("cool");
 console.log(e);
+this.prior = e;
+var node = document.getElementById(e.id);
+console.log(node.offsetTop);
+
 }
 
 function get_data(){
