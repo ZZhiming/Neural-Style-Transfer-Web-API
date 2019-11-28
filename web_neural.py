63eae95e2bc9a1f054565b700279df5b1a6f2476
Zhiming Zhang <zhangzm05@gmail.com>
2019-11-27 23:18:56 -0500

    upload style support

--- web_neural.py
+++ web_neural.py
@@ -45,6 +45,29 @@ def upload_file():
                                     filename=filename))
     return render_template("home_page.html")
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_style():
+    print("received ... ")
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            st.get_stlyed(filename)
+            #time.sleep(20)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template("home_page.html")
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
